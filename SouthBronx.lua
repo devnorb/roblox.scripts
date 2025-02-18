930ebfe52a59ffcf52832022b55018136bdceac8
norbyx1 <127006485+devnorb@users.noreply.github.com>
2025-02-17 17:57:03 -0800

    Create SouthBronx.lua

---
+++ SouthBronx.lua
@@ -0,0 +1,22 @@
+--[[
+South Bronx AntiCheat bypass by @norbyv1
+put this script in autoexec or it will NOT work
+]]--
+
+for i, v in getconnections(game.ScriptContext.Error) do
+    hookfunction(v.Function, function() end)
+end
+
+local old
+old = hookfunction(getfenv, newcclosure(function(...)
+    if not checkcaller() then
+        for i, v in pairs(getscriptclosure(getcallingscript())) do
+            hookfunction(v, function() return task.wait(9e9) end)
+        end
+        return task.wait(9e9)
+    end
+    return old(...)
+end))
+
+print("south bronx anticheat bypassed")
+print("norby was here")
